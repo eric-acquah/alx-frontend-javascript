3581fd64e566141087fd3ace49be05b65e4dcf5b
Eric Acquah <ericacquah1720@gmail.com>
2024-03-05 14:29:53 +0000

    Constants and let variables

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst(){
+    const task = "I prefer const when I can.";
+    return task;
+}
+
+export function getlast(){
+    return ' is okay';
+}
+
+export function taskNext(){
+    let combination = 'But sometimes let';
+    combination += getlast();
+
+    return combination;
+}
