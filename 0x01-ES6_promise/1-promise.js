fdf4c136f365ff7df03e33f1561d9212c8fe3389
Eric Acquah <ericacquah1720@gmail.com>
2024-03-07 16:32:06 +0000

    Promise resolve and reject

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+/* Returns a promise with either a resolve or reject value */
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
