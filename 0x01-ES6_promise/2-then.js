39be3430d9ec78c888ae7348064d45a936202ab3
Eric Acquah <ericacquah1720@gmail.com>
2024-03-07 17:35:58 +0000

    promise hadlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -7,7 +7,7 @@ export default function handleResponseFromAPI(promise) {
   };
 
   promise
-    .then(console.log('Got a response from the API'))
     .then(() => successObj)
+    .then(console.log('Got a response from the API'))
     .catch(Error());
 }
