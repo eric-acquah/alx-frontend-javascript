e66a28f09bcc3f78504896df1224b2de1e95fcd6
Eric Acquah <ericacquah1720@gmail.com>
2024-03-08 13:57:29 +0000

    Resolve multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,6 +6,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((utils) => {
       console.log(utils[0].body, utils[1].firstName, utils[1].lastName);
+      return utils;
     })
     .catch(() => console.log('Signup system offline'));
 }
