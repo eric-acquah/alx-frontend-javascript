5f38af4dae3a896b7afc8bb2a8601316b2673ecd
Eric Acquah <ericacquah1720@gmail.com>
2024-03-08 14:43:10 +0000

    fixing multiple promise resolve

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,9 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((utils) => console.log(utils[0].body, utils[1].firstName, utils[1].lastName))
+    .then((utils) => {
+      console.log(utils[0].body, utils[1].firstName, utils[1].lastName);
+      return `${utils[0].body} ${utils[1].firstName} ${utils[1].lastName}`;
+    })
     .catch(() => console.log('Signup system offline'));
 }
