06920d39291592d1daf5c3c90bc411ef6ef4dbf4
Eric Acquah <ericacquah1720@gmail.com>
2024-03-08 14:00:32 +0000

    Resolve multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 /* Resolves returned promises */
 
+const p1 = uploadPhoto();
+const p2 = createUser();
+
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([p1, p2])
     .then((utils) => {
       console.log(utils[0].body, utils[1].firstName, utils[1].lastName);
       return utils;
