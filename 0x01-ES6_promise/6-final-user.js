25684a416a3bb2a8a01fef2e59d07ff117706736
Eric Acquah <ericacquah1720@gmail.com>
2024-03-08 16:07:31 +0000

    Multiple resolve

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((all) => all);
 }
