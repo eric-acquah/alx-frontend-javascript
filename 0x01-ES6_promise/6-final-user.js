bfe8b3d6814c335c50e413e6e4b008f4e08826e2
Eric Acquah <ericacquah1720@gmail.com>
2024-03-08 22:46:25 +0000

    Fixing code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,18 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((allResolve) => [allResolve]);
+    .then((allResolve) => {
+      allResolve.map((status, value, reason) => {
+        if (status === 'fulfilled') {
+          return {
+            status,
+            value,
+          };
+        }
+        return {
+          status,
+          value: `${reason.name}: ${reason.message}`,
+        };
+      });
+    });
 }
