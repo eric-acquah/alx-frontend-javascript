2a8f12d212c74712108259a580e8578aa5cb7498
Eric Acquah <ericacquah1720@gmail.com>
2024-03-15 13:10:02 +0000

    Interface with index signature

---
+++ 0x04-TypeScript/task_1/main.ts
@@ -0,0 +1,10 @@
+/* Creates a Teacher interface */
+
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
