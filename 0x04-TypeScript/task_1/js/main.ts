e9fcf2a88b0ad23a5e9e42e3f061052115279c32
Eric Acquah <ericacquah1720@gmail.com>
2024-03-15 18:08:49 +0000

    Class and constructor interfaces

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -20,3 +20,42 @@ interface printTeacherFunction {
 let printTeacher: printTeacherFunction = function(firstName: string, lastName: string) {
     return `${firstName[0]}. ${lastName}`;
 }
+
+// Interface for StudentClass
+interface studentClassTemp {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface constructorTemp {
+    new(firstName: string, lastName: string): StudentClass;
+}
+
+function createNewStudent(struct: constructorTemp, fname: string, lname: string): studentClassTemp {
+    return new struct(fname, lname);
+}
+
+
+// Class definition
+class StudentClass implements studentClassTemp {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// const student11 = createNewStudent(StudentClass, 'Joe', 'Kind');
+
+// console.log(student11.displayName())
+// console.log(student11.workOnHomework())
